4c2e9150c97ef48fdcd326afe3f70308c0a4f900
= <ChanakanThongrong@gmail.com>
2026-03-10 02:39:36 +0700

    feat(openapi): add openapi

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import { Elysia } from "elysia";
 import { resource } from "./resource";
 import { openapi } from '@elysiajs/openapi'
 
-const app = new Elysia().use(openapi({
-	path: '/v1/openapi'
-})).get("/health", () => "Hello, it's working").use(resource).listen(3000);
+const app = new Elysia().use(openapi()).get("/health", () => "Hello, it's working").use(resource).listen(3000);
 
 
 console.log(
