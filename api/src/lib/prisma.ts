13c862c55fbfbc9c0472c7431ca5b19c0519152e
= <ChanakanThongrong@gmail.com>
2026-03-10 21:26:26 +0700

    feat(prisma): add logging

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,9 +1,12 @@
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
+const connectionString = `${process.env.DATABASE_URL}`;
+console.log(connectionString)
 const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL!,
+	connectionString,
 });
 
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter, log: ["query", "error", "warn"] });
 
