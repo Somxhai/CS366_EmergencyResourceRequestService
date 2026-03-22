5cf0834bf2e9f5fcceff320ac589e2f0fa5b98bf
= <ChanakanThongrong@gmail.com>
2026-03-22 18:38:56 +0700

    feat(index): add env port

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,8 +2,10 @@ import { Elysia } from "elysia";
 import { resource } from "./resource";
 import { openapi } from '@elysiajs/openapi'
 
-const app = new Elysia().use(openapi()).get("/health", () => "Hello, it's working").use(resource).listen(3000);
-
+const app = new Elysia().use(openapi()).get("/health", () => "Hello, it's working").use(resource).listen({
+	port: process.env.PORT || 3000,
+	hostname: "0.0.0.0"
+});
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
