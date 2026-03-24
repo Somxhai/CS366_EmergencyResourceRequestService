50d2f2d4606da07e8f4615b4c3ffb86127ee5e93
= <ChanakanThongrong@gmail.com>
2026-03-24 10:47:21 +0700

    fix: db credentials and bugs

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,12 +1,22 @@
 import 'dotenv/config'; // make sure to install dotenv package
 import { defineConfig } from 'drizzle-kit';
+import fs from "fs";
+
+const ca = fs.readFileSync("./cert/global-bundle.pem").toString();
 export default defineConfig({
 	dialect: 'postgresql',
 	out: './drizzle',
-	schema: './drizzle/schema.ts',
+	schema: './src/db/schema.ts',
 	dbCredentials: {
-		url: process.env.DATABASE_URL!,
-		ssl: 'require'
+		host: process.env.DB_HOST!,
+		user: process.env.DB_USER!,
+		password: process.env.DB_PASSWORD!,
+		database: process.env.DB_DATABASE!,
+		// url: process.env.DATABASE_URL!,
+		ssl: {
+			ca,
+			rejectUnauthorized: false
+		}
 	},
 });
 
--- api/src/lib/db.ts
+++ api/src/lib/db.ts
@@ -1,5 +1,22 @@
 
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import fs from "fs";
+import path from "path";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const certPath = path.resolve(process.cwd(), 'cert/global-bundle.pem');
+const ca = fs.readFileSync(certPath);
+export const db = drizzle({
+	connection: {
+
+		host: process.env.DB_HOST!,
+		user: process.env.DB_USER!,
+		password: process.env.DB_PASSWORD!,
+		database: process.env.DB_DATABASE!,
+		// connectionString: process.env.DATABASE_URL!,
+		ssl: {
+			ca: ca.toString(),
+			rejectUnauthorized: false
+		}
+	}
+});
