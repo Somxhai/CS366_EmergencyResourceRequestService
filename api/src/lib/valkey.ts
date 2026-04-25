8718be889daf66ae8c04f4abd776d4b077a2ab1b
= <ChanakanThongrong@gmail.com>
2026-04-25 18:25:10 +0700

    feat: add credentials

--- api/src/lib/valkey.ts
+++ api/src/lib/valkey.ts
@@ -1,5 +1,13 @@
 import { GlideClient } from "@valkey/valkey-glide";
 
 export const valkeyClient = await GlideClient.createClient({
-	addresses: [{ host: process.env.VALKEY_ENDPOINT ?? "", port: Number(process.env.VALKEY_PORT) || 6379 }], useTLS: false
+	addresses: [{
+		host: process.env.VALKEY_ENDPOINT ?? "",
+		port: Number(process.env.VALKEY_PORT) || 6379
+	}],
+	useTLS: false,
+	credentials: {
+		username: "default",
+		password: process.env.VALKEY_PASSWORD ?? ""
+	}
 });
